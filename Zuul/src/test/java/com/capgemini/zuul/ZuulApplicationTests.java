64ed84d2f2d4db85d0b09053a005a301cda43ab4
tclauss <thomas.clauss@capgemini.com>
2020-12-04 10:51:46 +0100

    Zuul added

---
+++ Zuul/src/test/java/com/capgemini/zuul/ZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgemini.zuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
